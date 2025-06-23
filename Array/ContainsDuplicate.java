dd7447693e8f3deb8f2aca87bd71b9794979527c
ParidhiPatidar <155944859+Paridhipatidar@users.noreply.github.com>
2025-06-23 23:24:44 +0530

    Create ContainsDuplicate.java

---
+++ Array/ContainsDuplicate.java
@@ -0,0 +1,14 @@
+public class Solution {
+public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true; // Duplicate found
+            }
+            seen.add(num);
+        }
+
+        return false; // All elements are unique
+    }
+}
