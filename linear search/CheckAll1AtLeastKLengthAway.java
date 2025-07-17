32162bd0c402cb196e55ccf442f7f9f724ade14b
ParidhiPatidar <155944859+Paridhipatidar@users.noreply.github.com>
2025-07-17 22:59:17 +0530

    Create CheckAll1AtLeastKLengthAway.java

---
+++ linear search/CheckAll1AtLeastKLengthAway.java
@@ -0,0 +1,14 @@
+class CheckAll1AtLeastKLengthAway{
+    public boolean kLengthApart(int[] nums, int k) {
+        int prev = -1; // to store index of last 1
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                if (prev != -1 && i - prev - 1 < k) {
+                    return false;
+                }
+                prev = i;
+            }
+        }
+        return true;
+    }
+}
