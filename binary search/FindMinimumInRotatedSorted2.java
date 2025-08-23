431f27749f9f25e70042ee3e657522ada4bce4d0
ParidhiPatidar <155944859+Paridhipatidar@users.noreply.github.com>
2025-08-23 23:36:57 +0530

    Create FindMinimumInRotatedSorted2.java

---
+++ binary search/FindMinimumInRotatedSorted2.java
@@ -0,0 +1,22 @@
+class FindMinimumInRotatedSorted2 {
+    public int findMin(int[] nums) {
+        int low = 0, high = nums.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > nums[high]) {
+                // Minimum must be in right half
+                low = mid + 1;
+            } else if (nums[mid] < nums[high]) {
+                // Minimum could be at mid or left half
+                high = mid;
+            } else {
+                // nums[mid] == nums[high], shrink range
+                high--;
+            }
+        }
+
+        return nums[low]; // or nums[high], since low == high
+    }
+}
