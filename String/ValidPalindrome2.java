9b1da40ca3abc94284b18f0e483582dbe98ebb00
ParidhiPatidar <155944859+Paridhipatidar@users.noreply.github.com>
2025-10-04 10:59:53 +0530

    Create ValidPalindrome2.java

---
+++ String/ValidPalindrome2.java
@@ -0,0 +1,23 @@
+class ValidPalindrome2 {
+    public boolean validPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                // Try skipping one character (either left or right)
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+
+        return true; // It's already a palindrome
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left++) != s.charAt(right--)) return false;
+        }
+        return true;
+    }
+}
