1af969d23a026afc4988fdaf9e3a7ef4eb81fcbe
ParidhiPatidar <155944859+Paridhipatidar@users.noreply.github.com>
2025-11-03 18:31:26 +0530

    Create PrintOneToN.java

---
+++ Recursion/PrintOneToN.java
@@ -0,0 +1,11 @@
+class PrintOneToN{
+        public static void main(String args[]){
+       funrev(5);
+    }
+    static void funrev(int n){
+        if(n==0)
+        return;
+        funrev(n-1);
+        System.out.println(n);
+    }
+}
